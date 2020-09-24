50c8ba0bf1640d6ac7fdf967b0ac53ebf3b79f40
hmartin28 <hingyimartin@gmail.com>
2020-09-24 14:43:56 +0200

    Modify the script

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./$1 units/monster1.json units/monster2.json > $2
-./$1 units/monster1.json units/monster3.json >> $2
+./$1 units/monster1.json units/monster2.json > results.txt
+./$1 units/monster1.json units/monster3.json >> results.txt
 
-./$1 units/monster2.json units/monster1.json >> $2
-./$1 units/monster2.json units/monster3.json >> $2
+./$1 units/monster2.json units/monster1.json >> results.txt
+./$1 units/monster2.json units/monster3.json >> results.txt
 
-./$1 units/monster3.json units/monster1.json >> $2
-./$1 units/monster3.json units/monster2.json >> $2
+./$1 units/monster3.json units/monster1.json >> results.txt
+./$1 units/monster3.json units/monster2.json >> results.txt
