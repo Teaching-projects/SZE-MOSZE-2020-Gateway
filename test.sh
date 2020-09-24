570bfe7196f700616a9b5908f1dce3cc98cf360e
hmartin28 <hingyimartin@gmail.com>
2020-09-24 15:08:19 +0200

    Fix the test script

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./$1 units/monster1.json units/monster2.json > results.txt
-./$1 units/monster1.json units/monster3.json >> results.txt
+./a.out units/monster1.json units/monster2.json > results.txt
+./a.out units/monster1.json units/monster3.json >> results.txt
 
-./$1 units/monster2.json units/monster1.json >> results.txt
-./$1 units/monster2.json units/monster3.json >> results.txt
+./a.out units/monster2.json units/monster1.json >> results.txt
+./a.out units/monster2.json units/monster3.json >> results.txt
 
-./$1 units/monster3.json units/monster1.json >> results.txt
-./$1 units/monster3.json units/monster2.json >> results.txt
+./a.out units/monster3.json units/monster1.json >> results.txt
+./a.out units/monster3.json units/monster2.json >> results.txt
