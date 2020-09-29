6b5d8ba6f5418ec18b9a2318e21ab1cab375a76a
hmartin28 <hingyimartin@gmail.com>
2020-09-29 15:36:48 +0200

    Modify the monsterAttack funciton

--- monster.cpp
+++ monster.cpp
@@ -8,10 +8,10 @@ int Monster::getHp() const { return hp; }
 
 int Monster::getDmg() const { return dmg; }
 
-void Monster::hpDown(int dmg) { hp -= dmg; }
+// void Monster::hpDown(int dmg) { hp -= dmg; }
 
-void Monster::monsterAttack(Monster &monster) {
-  if (hp > 0) monster.hpDown(dmg);
+void Monster::monsterAttack(Monster &target) const {
+  if (hp > 0) target.hp -= getDmg();
 }
 
 bool Monster::isDefeated() const {
