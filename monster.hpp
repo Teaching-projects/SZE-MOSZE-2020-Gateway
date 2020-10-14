bd5aaf287d9647d7a421eea1d7ba36afd01b04fc
hmartin28 <hingyimartin@gmail.com>
2020-10-14 15:08:56 +0200

    Modify for the doxygen

--- monster.hpp
+++ monster.hpp
@@ -27,20 +27,30 @@ private:
   const double cd; ///< The cooldown of the monster
   double cd_c = 0; ///< The cooldown counter of the monster
 
-  void monsterAttack(Monster &target) const; /// This funcitons decerease a monsters health by the other monsters damage
+  /// This funcitons decerease a monsters health by the other monsters damage
+  void monsterAttack(Monster &target) const;
 
 public:
 
-  Monster(const std::string &n, int &h, const int &d, const double c) : name(n), hp(h), dmg(d), cd(c){} /// This is the constructor
-  std::string getName() const; /// This is a simple getter for a monsters name
-  int getHp() const; /// This is a simple getter for a monsters name health points
-  int getDmg() const; /// This is a simple getter for a monsters name damage
-  double getCd() const; /// This is a simple getter for a monsters cooldown
-  double getCd_c() const; /// This is a simple getter for a monster cooldown counter
+  /// This is the constructor
+  Monster(const std::string &n, int &h, const int &d, const double c) : name(n), hp(h), dmg(d), cd(c){}
+  /// This is a simple getter for a monsters name
+  std::string getName() const;
+  /// This is a simple getter for a monsters name health points
+  int getHp() const;
+  /// This is a simple getter for a monsters name damage
+  int getDmg() const;
+  /// This is a simple getter for a monsters cooldown
+  double getCd() const;
+  /// This is a simple getter for a monster cooldown counter
+  double getCd_c() const;
 
-  bool monsterCd(Monster &target); /// This is a funciton to decrease the cooldown of a monster
-  bool isDefeated() const; /// This funciton decides if a monster is defeated (health is lower or equal zero)
-  static Monster readJson(const std::string &filename); /// This is a function to read the monsters attributes
+  /// This is a funciton to decrease the cooldown of a monster
+  bool monsterCd(Monster &target);
+  /// This funciton decides if a monster is defeated (health is lower or equal zero
+  bool isDefeated() const;
+  /// This is a function to read the monsters attributes
+  static Monster readJson(const std::string &filename);
 
 };
 
