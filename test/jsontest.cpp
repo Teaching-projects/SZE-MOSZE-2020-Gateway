ccd4f8ac693e1ba6be641f17af4df0e587ecf29e
hmartin28 <hingyimartin@gmail.com>
2020-10-16 22:46:06 +0200

    Make the unit tests

--- test/jsontest.cpp
+++ test/jsontest.cpp
@@ -1,5 +1,37 @@
-#include"../parser.hpp"
-#include<gtest/gtest.h>
+#include "../parser.hpp"
+#include <gtest/gtest.h>
+
+TEST(jsontest, wrong_input_test) {
+
+  Parser test;
+
+  ASSERT_THROW(test.fileparser("badmonster1.json"),std::invalid_argument);
+
+};
+
+TEST(jsontest, string_diff_test) {
+
+  Parser test;
+
+  test.fileparser("monster1.json");
+
+  ASSERT_NE(test.readstring("name"),"Rogi");
+
+};
+
+TEST(jsontest,fileparser) {
+
+  Parser test;
+
+  test.fileparser("monster1.json");
+
+  ASSERT_EQ(test.readstring("name"), "Igor");
+
+  ASSERT_EQ(test.readint("hp"), 200);
+
+  ASSERT_EQ(test.readint("dmg"), 40);
+
+};
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
