0c2fd62c3248da4b0d080efc7f621983f38d54fb
Bitchka98 <49394361+Bitchka98@users.noreply.github.com>
2020-10-25 00:05:51 +0200

    Add files via upload

--- test/jsontest.cpp
+++ test/jsontest.cpp
@@ -1,47 +1,36 @@
 #include "../parser.hpp"
 #include <gtest/gtest.h>
 
-TEST(jsontest, wrong_input_test) {
+TEST(JsonTest, Filetest)
+{
+    ASSERT_THROW(Json::Fileparse("badmonster1.json"), std::runtime_error);
+    ASSERT_NO_THROW(Json::Fileparse("../unit/monster1.json"));
+}
+TEST(JsonTest, Stringparse)
+{
+    std::map<std::string, std::string> data = Json::Stringparse("{    \"name\" : \"Bicska\",    \"hp\" : 45000,    \"dmg\" : 1000}");
+    ASSERT_EQ(data["name"], "Bicska");
+    ASSERT_EQ(data["hp"], "45000");
+    ASSERT_EQ(data["dmg"], "1000");
+}
+TEST(JsonTest, Valid)
+{
+    ASSERT_THROW(Json::Valid("{    name\" : \"Bicska\",    \"hp\" : 45000,    \"dmg\" : 1000 }"), std::runtime_error);
+    ASSERT_THROW(Json::Valid("{    name\" : \"Bicska\",    \"hp\" : 45000,    \"dmg\" : 1000 }"), std::runtime_error);
+    ASSERT_THROW(Json::Valid("{    name\" : \"Bicska\",    \"hp\" : 45000,    \"dmg\" : 1000 }"), std::runtime_error);
+    ASSERT_THROW(Json::Valid("{    name\" : \"Bicska\",    \"hp\" : 45000,    \"dmg\" : 1000 }"), std::runtime_error);
+    ASSERT_THROW(Json::Valid("{    name\" : \"Bicska\",    \"hp\" : 45000,    \"dmg\" : 1000 }"), std::runtime_error);
+    ASSERT_THROW(Json::Valid("{    name\" : \"Bicska\",    \"hp\" : 45000,    \"dmg\" : 1000 }"), std::runtime_error);
+    ASSERT_THROW(Json::Valid("{    name\" : \"Bicska\",    \"hp\" : 45000,    \"dmg\" : 1000 }"), std::runtime_error);
+    ASSERT_THROW(Json::Valid("{    name\" : \"Bicska\",    \"hp\" : 45000,    \"dmg\" : 1000 }"), std::runtime_error);
+    ASSERT_THROW(Json::Valid("{    name\" : \"Bicska\",    \"hp\" : 45000,    \"dmg\" : 1000 }"), std::runtime_error);
+    ASSERT_THROW(Json::Valid("{    name\" : \"Bicska\",    \"hp\" : 45000,    \"dmg\" : 1000 }"), std::runtime_error);
+    ASSERT_THROW(Json::Valid("{    name\" : \"Bicska\",    \"hp\" : 45000,    \"dmg\" : 1000 }"), std::runtime_error);
+    ASSERT_NO_THROW(Json::Valid("{    \"name\" : \"Bicska\",    \"hp\" : 45000,    \"dmg\" : 1000}"));
+}
 
-  Parser test;
-
-  ASSERT_THROW(test.fileparser("badmonster1.json"),std::invalid_argument);
-
-};
-
-TEST(jsontest, diff_test) {
-
-  Parser test;
-
-  Parser test2;
-
-  test.fileparser("monster1.json");
-
-  test2.fileparser("monster2.json");
-
-  ASSERT_NE(test.readstring("name"),test2.readstring("name"));
-
-  ASSERT_NE(test.readint("hp"),test2.readint("hp"));
-
-  ASSERT_NE(test.readint("dmg"),test2.readint("dmg"));
-
-};
-
-TEST(jsontest,fileparser) {
-
-  Parser test;
-
-  test.fileparser("monster1.json");
-
-  ASSERT_EQ(test.readstring("name"), "Igor");
-
-  ASSERT_EQ(test.readint("hp"), 200);
-
-  ASSERT_EQ(test.readint("dmg"), 40);
-
-};
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
