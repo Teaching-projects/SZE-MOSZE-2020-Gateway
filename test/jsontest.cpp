c1459ee5ad501cd2ef544eb11770d32c09a19fba
Bitchka98 <49394361+Bitchka98@users.noreply.github.com>
2020-10-26 12:04:50 +0100

    jsontest.cpp repair many

--- test/jsontest.cpp
+++ test/jsontest.cpp
@@ -3,7 +3,7 @@
 
 TEST(ParserTest, Filetest)
 {
-    ASSERT_THROW(Parser::Fileparse("badmonster1.json"), std::runtime_error);
+    ASSERT_THROW(Parser::Fileparse("xy.json"), std::runtime_error);
     ASSERT_NO_THROW(Parser::Fileparse("../unit/monster1.json"));
 }
 TEST(ParserTest, Stringparse)
