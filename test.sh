f14306e1fb647ee1af3107fda9980be122f0d3a8
hmartin28 <hingyimartin@gmail.com>
2020-12-06 09:06:37 +0100

    More changes...

--- test.sh
+++ test.sh
@@ -3,8 +3,8 @@
 ./a.out units/Eivar.json units/Sigurd.json > results.txt
 ./a.out units/Eivar.json units/Ubba.json >> results.txt
 
-./a.out units/Sigurd.json units/monster1.json >> results.txt
+./a.out units/Sigurd.json units/Eivar.json >> results.txt
 ./a.out units/Sigurd.json units/Ubba.json >> results.txt
 
-./a.out units/Ubba.json units/monster1.json >> results.txt
+./a.out units/Ubba.json units/Eivar.json >> results.txt
 ./a.out units/Ubba.json units/Sigurd.json >> results.txt
