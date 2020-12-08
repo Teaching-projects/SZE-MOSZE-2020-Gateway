de5242a5f0f95702d8b770b73ded41c8adab26b0
hmartin28 <hingyimartin@gmail.com>
2020-12-08 22:14:36 +0100

    Fixing and adding more test

--- test.sh
+++ test.sh
@@ -2,15 +2,25 @@
 
 ./a.out units/Eivar.json units/Sigurd.json > results.txt
 ./a.out units/Eivar.json units/Ubba.json >> results.txt
+./a.out units/Eivar.json units/Bjorn.json >> results.txt
+./a.out units/Eivar.json units/Ragnar.json >> results.txt
 
 ./a.out units/Sigurd.json units/Eivar.json >> results.txt
 ./a.out units/Sigurd.json units/Ubba.json >> results.txt
+./a.out units/Sigurd.json units/Bjorn.json >> results.txt
+./a.out units/Sigurd.json units/Ragnar.json >> results.txt
 
 ./a.out units/Ubba.json units/Eivar.json >> results.txt
 ./a.out units/Ubba.json units/Sigurd.json >> results.txt
+./a.out units/Ubba.json units/Bjorn.json >> results.txt
+./a.out units/Ubba.json units/Ragnar.json >> results.txt
 
 ./a.out units/Bjorn.json units/Eivar.json >> results.txt
 ./a.out units/Bjorn.json units/Sigurd.json >> results.txt
+./a.out units/Bjorn.json units/Ubba.json >> results.txt
+./a.out units/Bjorn.json units/Ragnar.json >> results.txt
 
 ./a.out units/Ragnar.json units/Eivar.json >> results.txt
 ./a.out units/Ragnar.json units/Sigurd.json >> results.txt
+./a.out units/Ragnar.json units/Ubba.json >> results.txt
+./a.out units/Ragnar.json units/Bjorn.json >> results.txt
